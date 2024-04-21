7dbb5d4afb4cbf43a367e167d07ccd7e3d1cfce4
anastaishafilonova <filonova.ai@phystech.edu>
2024-04-21 13:06:52 +0300

    changed annotations

--- book-service/src/main/java/book/service/service/BookService.java
+++ book-service/src/main/java/book/service/service/BookService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 
-@Component
+@Service
 public class BookService {
   private BookRepository bookRepository;
   private AuthorService authorService;
